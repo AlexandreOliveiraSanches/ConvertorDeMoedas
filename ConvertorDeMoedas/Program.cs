293b061a08a6dfa1b6def64f0044443410078c76
Alexandre Oliveira <ao.sanches.ao@gmail.com>
2023-08-10 19:08:53 -0300

    Convertor de dólar para real

---
+++ ConvertorDeMoedas/Program.cs
@@ -0,0 +1,23 @@
+﻿using ConvertorDeMoedas;
+using System;
+using System.Globalization;
+
+namespace ConvertorDeMoedas
+{
+    class Program {
+        static void Main(string[] args) {
+
+            
+
+            Console.WriteLine("Qual é a cotação do dólar? ");
+            double cotacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Quantos dólares você vai comprar? ");
+            double quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double result = ConvertorDeMoedas.DolarParaReal(quantia, cotacao);
+            Console.WriteLine("Valor a ser pago em reais: " + result.ToString("F2", CultureInfo.InvariantCulture));
+            
+        }
+    }
+}
